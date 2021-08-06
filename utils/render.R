ea64fb544b8a202e4d56d5461fb9f0cd14c9c59f
Tam-Pham <phamttam17@gmail.com>
2021-08-06 19:22:26 +0800

    remove build workflow and render only html

--- utils/render.R
+++ utils/render.R
@@ -6,14 +6,14 @@ if(!file.exists("index.Rmd") && file.exists("../index.Rmd")) {
 }
 
 # Render as README
-rmarkdown::render('index.Rmd', output_format = rmarkdown::md_document(variant = "markdown_github"), output_file = "README")
+# rmarkdown::render('index.Rmd', output_format = rmarkdown::md_document(variant = "markdown_github"), output_file = "README")
 
 # Render HTML
 rmarkdown::render('index.Rmd', output_format = "html_document", output_dir = "docs/", output_file = "index")
 
 # Render Word
-rmarkdown::render('index.Rmd', output_format = "word_document", output_dir = "word_and_pdf/", output_file = "SupplementaryMaterials")
+# rmarkdown::render('index.Rmd', output_format = "word_document", output_dir = "word_and_pdf/", output_file = "SupplementaryMaterials")
 
 # Render PDF
-rmarkdown::render('index.Rmd', output_format = "pdf_document", output_dir = "word_and_pdf/", output_file = "SupplementaryMaterials")
+# rmarkdown::render('index.Rmd', output_format = "pdf_document", output_dir = "word_and_pdf/", output_file = "SupplementaryMaterials")
 
