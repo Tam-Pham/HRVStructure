9963c1a5b14703b93f9b708666f41da8da0a8403
Tam-Pham <phamttam17@gmail.com>
2021-09-23 15:15:09 +0800

    update analysis scripts

--- data/make_data.R
+++ data/make_data.R
@@ -9,29 +9,29 @@ data_GG <- data_GG %>%
   mutate(Recording_Length = rep(8, times = nrow(data_GG))) %>% 
   filter(Participant != 'S200122')
 
-data_Fiction <- read.csv("data_Fiction_RestingState.csv", stringsAsFactors = FALSE)
-data_Fiction <- data_Fiction %>% 
-  select(starts_with('HRV_'), Participant) %>% 
-  mutate(Database = "Fiction_RestingState") %>% 
-  mutate(Recording_Length = rep(8, times = nrow(data_Fiction)))
+# data_Fiction <- read.csv("data_Fiction_RestingState.csv", stringsAsFactors = FALSE)
+# data_Fiction <- data_Fiction %>% 
+#   select(starts_with('HRV_'), Participant) %>% 
+#   mutate(Database = "Fiction_RestingState") %>% 
+#   mutate(Recording_Length = rep(8, times = nrow(data_Fiction)))
 
 # Combine data
 data_combined <- rbind(data_library, data_GG)
-data_combined <- rbind(data_combined, data_Fiction)
+# data_combined <- rbind(data_combined, data_Fiction)
 
 write.csv(data_combined, "data_combined.csv", row.names = FALSE)
 
 
-hrv_cols <- names(select(data_combined, -Database, -Recording_Length, -Participant))
-
-nans <- data_combined %>% 
-  group_by(Database) %>%
-  select(one_of(hrv_cols)) %>%
-  summarise_all(function(x) sum(is.na(x)) / n())
-
-nans <- nans[sapply(nans, function(x) if(is.character(x)) return(TRUE) else sum(x) > 0)]
-nans
-data_combined %>%
-  filter(is.na(HRV_RMSSD)) %>%
-  select(Participant, Database)
+# hrv_cols <- names(select(data_combined, -Database, -Recording_Length, -Participant))
+# 
+# nans <- data_combined %>% 
+#   group_by(Database) %>%
+#   select(one_of(hrv_cols)) %>%
+#   summarise_all(function(x) sum(is.na(x)) / n())
+# 
+# nans <- nans[sapply(nans, function(x) if(is.character(x)) return(TRUE) else sum(x) > 0)]
+# nans
+# data_combined %>%
+#   filter(is.na(HRV_RMSSD)) %>%
+#   select(Participant, Database)
 
